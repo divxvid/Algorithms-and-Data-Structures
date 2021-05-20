df721ce65c1740f5841056edf39fdcdb87db8930
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2021-05-20 15:55:32 +0530

    Added a simple DSU implementation.

---
+++ DataStructures/Disjoint_Set_Union/dsu.cpp
@@ -0,0 +1,46 @@
+struct DSU {
+    int sz ;
+    int* parent ;
+    long long* weight ;
+
+    DSU(int n) {
+        sz = n+1 ;
+        parent = new int[sz] ;
+        weight = new long long[sz] ;
+
+        for (int i = 0; i < sz; ++i) {
+            parent[i] = i ;
+            weight[i] = 0 ;
+        }
+    }
+
+    void get_parent(int i) {
+        while (parent[i] != i) {
+            i = parent[i] ;
+        }
+        return i ;
+    }
+
+    void find(int a, int b) {
+        return get_parent(a) == get_parent(b) ;
+    }
+
+    void union_(int a, int b) {
+        int _pa = get_parent(a) ;
+        int _pb = get_parent(b) ;
+
+        if (_pa == _pb) return ;
+        if (weight[_pa] >= weight[_pb]) {
+            parent[_pb] = _pa ;
+            weight[_pa] += weight[_pb] ;
+        } else {
+            parent[_pa] = _pb ;
+            weight[_pb] += weight[_pa] ;
+        }
+    }
+
+    ~DSU() {
+        delete[] parent ;
+        delete[] weight ;
+    }
+};
